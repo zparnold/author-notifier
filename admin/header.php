04369c01452f321ad8d9a4b12d9a2e666610346c
Zach Arnold <me@zacharnold.org>
2016-01-17 14:50:32 -0800

    change author title

--- admin/header.php
+++ admin/header.php
@@ -5,8 +5,8 @@
 	<div class="nnr-logo"></div>
 
 	<div class="nnr-product-details">
-		<span class="nnr-product-name"><?php _e('Post Status Notifications', self::$text_domain); ?></span>
-		<span class="nnr-product-version"><?php echo WPSITE_POST_STATUS_NOTIFICATION_VERSION_NUM; ?></span>
+		<span class="nnr-product-name"><?php _e('Author Notifier', self::$text_domain); ?></span>
+		<span class="nnr-product-version"><?php echo AUTHOR_NOTIFIER_VERSION_NUM; ?></span>
 	</div>
 
 </div>
